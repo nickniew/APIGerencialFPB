285cd86d7c68b4ca386b631c156285d2012004ed
nickniew <nicolasvolz@gmail.com>
2024-03-19 17:45:51 -0300

    implementação de Swagger para testar API

--- src/main/java/nickniew/feedplayback/apigerencial/controller/Controller.java
+++ src/main/java/nickniew/feedplayback/apigerencial/controller/Controller.java
@@ -1,5 +1,6 @@
 package nickniew.feedplayback.apigerencial.controller;
 
+import io.swagger.annotations.ApiOperation;
 import java.util.Optional;
 import nickniew.feedplayback.apigerencial.service.Service;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Nickniew
  */
 @RestController
+@RequestMapping("/api")
 public abstract class Controller<T, ID> {
 
     private final Service<T, ID> servico;
@@ -26,12 +29,14 @@ public abstract class Controller<T, ID> {
     }
 
     @PostMapping
+    @ApiOperation(value = "cria registro")
     public ResponseEntity<T> create(@RequestBody T entity) {
         T savedEntity = servico.criarRegistro(entity);
         return new ResponseEntity<>(savedEntity, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
+    @ApiOperation(value = "atualiza registro")
     public ResponseEntity<T> update(@PathVariable ID id, @RequestBody T entity) {
         T updatedEntity = servico.atualizarRegistro(id, entity);
         if (updatedEntity != null) {
@@ -42,12 +47,14 @@ public abstract class Controller<T, ID> {
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation(value = "deleta registro")
     public ResponseEntity<Void> delete(@PathVariable ID id) {
         servico.excluirRegistro(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "busca registro por ID")
     public ResponseEntity<T> getById(@PathVariable ID id) {
         Optional<T> entity = servico.obterPorId(id);
         return entity.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
@@ -55,6 +62,7 @@ public abstract class Controller<T, ID> {
     }
 
     @GetMapping
+    @ApiOperation(value = "busca todos registros")
     public ResponseEntity<Iterable<T>> getAll() {
         Iterable<T> entities = servico.obterTodos();
         return new ResponseEntity<>(entities, HttpStatus.OK);
